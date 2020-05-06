f962b75c598a39ee120f317b4082d3625b77481a
Jeroen Claassens <support@favware.tech>
2020-05-06 22:44:00 +0200

    build: rmdir does recursive delete ya dummy me

--- packages/ts-config/scripts/postpublish.ts
+++ packages/ts-config/scripts/postpublish.ts
@@ -1,10 +1,9 @@
-import { remove, rmdir } from 'fs-nextra';
+import { rmdir } from 'fs-nextra';
 import { join, resolve } from 'path';
 
 async function postpublish() {
   const rootDir = resolve(__dirname, '..');
 
-  await remove(join(rootDir, 'dist', 'tsconfig.json'));
   await rmdir(join(rootDir, 'dist'));
 }
 
