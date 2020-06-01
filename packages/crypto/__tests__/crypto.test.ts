386750ee6b8ab5267c22a72500ffbb1886b15854
Jeroen Claassens <support@favware.tech>
2020-06-01 11:15:42 +0200

    test(crypto): fix unit tests for crypto lib

--- packages/crypto/__tests__/crypto.test.ts
+++ packages/crypto/__tests__/crypto.test.ts
@@ -1,42 +1,42 @@
 import cryptodefault, { crypto, Radix } from '../src';
 
 test('Base16 cryptography', () => {
-  const cryptography = crypto(280, Radix.base16);
+  const cryptography = crypto(280, Radix.Base16);
 
   expect(typeof cryptography).toBe('string');
   expect(cryptography).toHaveLength(70);
 });
 
 test('Base32 cryptography', () => {
-  const cryptography = crypto(280, Radix.base32);
+  const cryptography = crypto(280, Radix.Base32);
 
   expect(typeof cryptography).toBe('string');
   expect(cryptography).toHaveLength(56);
 });
 
 test('Base36 cryptography', () => {
-  const cryptography = crypto(280, Radix.base36);
+  const cryptography = crypto(280, Radix.Base36);
 
   expect(typeof cryptography).toBe('string');
   expect(cryptography).toHaveLength(55);
 });
 
 test('Base58 cryptography', () => {
-  const cryptography = crypto(280, Radix.base58);
+  const cryptography = crypto(280, Radix.Base58);
 
   expect(typeof cryptography).toBe('string');
   expect(cryptography).toHaveLength(48);
 });
 
 test('Base64 cryptography', () => {
-  const cryptography = crypto(280, Radix.base64);
+  const cryptography = crypto(280, Radix.Base64);
 
   expect(typeof cryptography).toBe('string');
   expect(cryptography).toHaveLength(47);
 });
 
 test('Check default export', () => {
-  const cryptography = cryptodefault(280, Radix.base16);
+  const cryptography = cryptodefault(280, Radix.Base16);
 
   expect(typeof cryptography).toBe('string');
   expect(cryptography).toHaveLength(70);
